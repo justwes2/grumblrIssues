c969d8bd54cf05855250fe5b56177bff57c6730c
Wes <wes.coffay@gmail.com>
2017-03-06 14:37:12 -0500

    issues with show.html

---
+++ js/app.js
@@ -0,0 +1,29 @@
+angular
+.module("grumblr", ["ui.router"])
+.config(["$stateProvider", RouterFunction])
+.controller("GrumbleIndexController", [GrumbleIndexControllerFunction])
+.controller("GrumbleShowController", ["$stateParams", GrumbleShowControllerFunction])
+
+function RouterFunction($stateProvider) {
+  $stateProvider
+  .state("grumbleIndex", {
+    url: "/grumbles",
+    controller: "GrumbleIndexController",
+    controllerAs: "vm",
+    templateUrl: "js/ng-views/index.html"
+  })
+  .state("grumbleShow", {
+    url: "/grumbles/:id",
+    controller: "GrumbleShowController",
+    controllerAs: "vm",
+    templeteUrl: "js/ng-views/show.html"
+  })
+}
+
+function GrumbleIndexControllerFunction() {
+  this.grumbles = grumbles;
+}
+
+function GrumbleShowControllerFunction($stateParams) {
+  this.grumble = grumbles[$stateParams.id];
+}
